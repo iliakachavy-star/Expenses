e69aa7a63499fcc6ccd2a5af53ffc8ef7732540f
Ilya <ilia.kachavy@gmail.com>
2026-06-07 16:04:01 +0300

    RelatedList

--- force-app/main/default/lwc/relatedList/relatedList.js
+++ force-app/main/default/lwc/relatedList/relatedList.js
@@ -1,3 +1,17 @@
+import getExpenses from '@salesforce/apex/ExpenseAppController.getExpenses';
 import { LightningElement } from 'lwc';
 
-export default class RelatedList extends LightningElement {}
+export default class RelatedList extends LightningElement {
+
+    async loadExpenses() {
+
+        try {
+            this.expenses = await getExpenses({
+                yearValue: this.selectedYear,
+                monthValue: this.selectedMonth
+            });
+        } catch (error) {
+            this.showError(error);
+        }
+    } 
+}
